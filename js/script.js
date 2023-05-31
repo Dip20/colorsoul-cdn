ff6440a6bfcc95cb5b0fcff2a2f92e89d586a9d9
Santu Sarkar <info.dip.sarkar@gmail.com>
2023-05-31 14:48:29 +0530

    init

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+(function ($) {
+  "use strict";
+
+    // jquery ready start fixed top nav sticky
+    $(document).ready(function() {
+      // jQuery code
+      ///////////////// fixed menu on scroll for desctop
+      if ($(window).width() > 992) {
+        var navbar_height = $('.navbar').outerHeight();
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 80) {
+            // if ($(this).scrollTop() > -0.1) {
+            $('.navbar-wrap').css('height', navbar_height + 'px');
+            $('#navbar_top').addClass("fixed-top");
+            $('#navbar_top').removeClass("d-none");
+    
+          } else {
+            $('#navbar_top').addClass("d-none");
+            $('#navbar_top').removeClass("fixed-top");
+            $('.navbar-wrap').css('height', 'auto');
+          }
+        });
+      } // end if
+    }); // jquery end sticky
+
+    
+})
+  (jQuery);
